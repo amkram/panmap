eee7f0e5638b0d91cb77d10c4f5cd9a8cd968c68
Alex <almkrame@ucsc.edu>
2024-01-12 03:41:54 -0800

    update

--- src/test/tree.test.cpp
+++ src/test/tree.test.cpp
@@ -16,32 +16,9 @@ using namespace tree;
 using namespace pmi;
 
 
-void getSequencesDFS(std::unordered_map<std::string, std::string> &result, mutableTreeData &data, Tree *T, const Node *node, const globalCoords_t &globalCoords) {
-
-    blockMutData_t blockMutData;
-    nucMutData_t nucMutData;
-
-    /*  Mutate with block and nuc mutations */
-    applyMutations(data, blockMutData, nucMutData, T, node, globalCoords);
-
-    /* Use current state of mutableTreeData to decode node's sequence */
-    std::string seq = tree::getAlignedSequence(data, T, node, true);
-
-    result[node->identifier] = seq;
-
-    /* Recursive step */
-    for(Node* child: node->children){
-        getSequencesDFS(result, data, T, child, globalCoords);
-    }
-
-    /* Undo mutations when backtracking */
-    seedIndex blank;
-    undoMutations(data, blank, T, node, blockMutData, nucMutData);
-}
-
 BOOST_AUTO_TEST_CASE(sequenceReconstruction) {
 
-    std::ifstream file("test.aligned.fa");
+    std::ifstream file("../dev/examples/sars2k.fa");
     std::unordered_map<std::string, std::string> expectedSequences;
 
     std::string id;
@@ -52,26 +29,18 @@ BOOST_AUTO_TEST_CASE(sequenceReconstruction) {
     }
 
     /* Panmat loading */
-    std::ifstream is("test.pmat");
+    std::ifstream is("../dev/examples/sars2k.pmat");
     boost::iostreams::filtering_streambuf<boost::iostreams::input> inPMATBuffer;
     inPMATBuffer.push(boost::iostreams::gzip_decompressor());
     inPMATBuffer.push(is);
     std::istream inputStream(&inPMATBuffer);
     PangenomeMAT::Tree *T = new PangenomeMAT::Tree(inputStream);
 
-    /* Setup */
-    tree::mutableTreeData data;
-    tree::globalCoords_t globalCoords;
-    setup(data, globalCoords, T);
-
-    std::unordered_map<std::string, std::string> resultSequences;
-    getSequencesDFS(resultSequences, data, T, T->root, globalCoords);
-
-    /* Verify that each sequence in a test tree matches both panmat lib's
-    ** getStringFromReference() and our DFS method's output
-    */
+    /* Get all sequences */
+    std::unordered_map<std::string, std::string> resultSequences = tree::getAllNodeStrings(T);
+  
+    /* Do they match expected sequences from aligned fasta? */
     for (auto r : expectedSequences) {
         BOOST_TEST(resultSequences[r.first] == expectedSequences[r.first]);
-        BOOST_TEST(resultSequences[r.first] == T->getStringFromReference(r.first));
     }
 }
