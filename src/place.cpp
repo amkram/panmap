3e48116b6550c41964beedd7954b559e98c48a85
genicos <nicoatnico.com@gmail.com>
2024-02-20 14:13:32 -0800

    Sam format sometimes, placement has a lot of debugging prints (ugly bad), some bugs remain

--- src/place.cpp
+++ src/place.cpp
@@ -112,7 +112,7 @@ void placeHelper(std::unordered_map<std::string, std::set<std::pair<int32_t, std
     placeDFS(&out, index, seedmers, scores, seedmerCounts, phyloCounts, T->root, T, "", nullptr);
 }
 
-seedmerIndex_t seedsFromFastq(std::ifstream &indexFile, int32_t *k, int32_t *s, int32_t *j, std::unordered_map<std::string, int32_t> &seedmerCounts, std::vector<std::string> &readSequences, std::vector<std::string> &readQuals, std::vector<std::string> &readNames, const std::string &fastqPath) {
+seedmerIndex_t seedsFromFastq(std::ifstream &indexFile, int32_t *k, int32_t *s, int32_t *j, std::unordered_map<std::string, int32_t> &seedmerCounts, std::vector<std::string> &readSequences, std::vector<std::string> &readQuals, std::vector<std::string> &readNames, std::vector<std::vector<seed>> &readSeedsFwd, std::vector<std::vector<seed>> &readSeedsBwd,  const std::string &fastqPath) {
     util::scopedTimer();
     FILE *fp;
     kseq_t *seq;
@@ -137,8 +137,13 @@ seedmerIndex_t seedsFromFastq(std::ifstream &indexFile, int32_t *k, int32_t *s, 
         std::string seq = readSequences[i];
         std::string name = readNames[i];
         std::string rc = reverseComplement(seq);
+
         std::vector<seed> syncmers = syncmerize(seq, tempK, tempS, false, true, 0);
+        readSeedsFwd.push_back(syncmers);
+
         std::vector<seed> syncmersReverse = syncmerize(rc, tempK, tempS, false, true, 0);
+        readSeedsBwd.push_back(syncmersReverse);
+
         std::vector<seedmer> seedmers = seedmerize(syncmers, tempJ);
         std::vector<seedmer> seedmersReverse = seedmerize(syncmersReverse, tempJ);  
         std::unordered_map<std::string, bool> seen;
@@ -187,6 +192,10 @@ seedmerIndex_t seedsFromFastq(std::ifstream &indexFile, int32_t *k, int32_t *s, 
     return seedmerIndex;
 }
 
+extern "C" {
+    void align_reads(const char *reference, int n_reads, const char **reads, const char **quality, const char **read_names, int *r_lens, int *seed_counts, uint8_t **reversed, int **ref_positions, int **qry_positions, char** sam_alignments);
+}
+
 void place::placeIsolate(std::ifstream &indexFile, const std::string &reads1Path, const std::string &reads2Path, Tree *T) {
     tree::mutableTreeData data;
     tree::globalCoords_t globalCoords;
@@ -197,10 +206,33 @@ void place::placeIsolate(std::ifstream &indexFile, const std::string &reads1Path
     std::vector<std::string> readSequences;
     std::vector<std::string> readQuals;
     std::vector<std::string> readNames;
+    std::vector<std::vector<seed>> readSeedsFwd;
+    std::vector<std::vector<seed>> readSeedsBwd;
     std::unordered_map<std::string, int32_t> seedmerCounts;
     int32_t k, s, j;
 
-    seedmerIndex_t seedmerIndex = seedsFromFastq(indexFile, &k, &s, &j, seedmerCounts, readSequences, readQuals, readNames, reads1Path);
+    seedmerIndex_t seedmerIndex = seedsFromFastq(indexFile, &k, &s, &j, seedmerCounts, readSequences, readQuals, readNames, readSeedsFwd, readSeedsBwd, reads1Path);
+    
+    /* Collecting forward and backward seeds into one vector */
+    std::vector<std::vector<seed>> readSeeds;
+    readSeeds.reserve(readSeedsFwd.size());
+    for(int i = 0; i < readSeedsFwd.size(); i++){
+        std::vector<seed> thisReadsSeeds;
+        thisReadsSeeds.reserve(readSeedsFwd[i].size() + readSeedsBwd[i].size());
+
+        for(int j = 0; j < readSeedsFwd[i].size(); j++) {
+            readSeedsFwd[i][j].reversed = false;
+            readSeedsFwd[i][j].pos = readSeedsFwd[i][j].pos + k - 1; // Minimap standard
+            thisReadsSeeds.push_back(readSeedsFwd[i][j]);
+        }
+        for(int j = 0; j < readSeedsBwd[i].size(); j++) {
+            readSeedsBwd[i][j].reversed = true;
+            readSeedsBwd[i][j].pos = readSeedsBwd[i][j].pos + k - 1; // Minimap standard
+            thisReadsSeeds.push_back(readSeedsBwd[i][j]);
+        }
+        readSeeds.push_back(thisReadsSeeds);
+    }
+
 
     /* Sample placement */
     std::cout << "⋌⋋ Placing sample ... " << std::flush;
@@ -243,7 +275,11 @@ void place::placeIsolate(std::ifstream &indexFile, const std::string &reads1Path
         }
         seedToRefPositions[seed].push_back(degap[refPos]);
     }
+    
     std::cout << "\n\nBest match: " << bestMatch << " (" << targetNodes[0].second << ")\n";
+    std::cout << bestMatchSequence << "\n";
+    std::cout << "\n\n\n\n\n\n\n\n\nn\n\\n\n\n\n\n\n";
+    std::cout << gappedSeq << "\n";
 
     /* Alignment to target */
 
@@ -253,4 +289,193 @@ void place::placeIsolate(std::ifstream &indexFile, const std::string &reads1Path
     **    - readSequences, readQuals, readNames contain the relevant info
     **    - seedToRefPositions maps a seed sequence (just seeds not seed-mers) to all matching positions in bestMatchSequence
     */
+
+
+    std::cout << "SIZE " << seedToRefPositions.size() << "\n";
+
+    std::vector<seed> refSeeds;
+    for(auto kv : seedToRefPositions) {
+        seed thisSeed;
+        thisSeed.seq = kv.first;
+        thisSeed.pos = kv.second[0];
+        refSeeds.push_back(thisSeed);
+    }
+
+    std::cout << "SIZE " << refSeeds.size() << "\n";
+
+    //Sorting ref seeds
+    sort(refSeeds.begin(), refSeeds.end(), [ ]( const seed& lhs, const seed& rhs )
+    {
+        return lhs.seq < rhs.seq ;
+    });
+
+    std::cout << "SIZE " << refSeeds.size() << "\n";
+
+
+    std::cerr << "REF SEEDS\n";
+    for(int i = 0; i < refSeeds.size(); i++) {
+        std::cerr << refSeeds[i].seq << "\n";
+    }
+
+    //Sorting read seeds
+    for(int i = 0; i < readSeeds.size(); i++){
+        sort(readSeeds[i].begin(), readSeeds[i].end(), [ ]( const seed& lhs, const seed& rhs )
+        {
+            return lhs.seq < rhs.seq;
+        });
+    }
+
+
+    /*
+    for(int i = 0; i < readSequences.size(); i++) {
+        std::cerr << "\n" << i <<"\n";
+        std::cerr << readSequences[i] << "\n";
+        std::cerr << readQuals[i] << "\n";
+        std::cerr << readSeeds[i].size() << "\n";
+        for(int j = 0; j < readSeeds[i].size() ; j++){
+            std::cerr << "X\n";
+            std::cerr << readSeeds[i][j].seq.size() << "\n";
+            std::cerr << readSeeds[i][j].seq << "\n";
+            std::cerr << readSeeds[i][j].pos << "\n";
+            std::cerr << readSeeds[i][j].reversed << "\n";
+        }
+    }
+    */
+
+    for(int i = 0; i < readSequences.size(); i++) {
+        std::cerr << "\n\n" << i << "\n";
+        for(int j = 0; j < readSeeds[i].size() ; j++){
+            std::cerr << readSeeds[i][j].seq << "\n";
+        }
+    }
+
+
+    //Finding syncmer matches
+    for(int r = 0; r < readSequences.size() ; r++){
+
+        int refSeedIndex = 0;
+        int readSeedIndex = 0;
+        std::cerr << "\n\n\n\n\n\n\n\n\n\n\nr " << r << "\n\n";
+
+        std::vector<seed> matchingSeeds;
+        while(readSeedIndex < readSeeds[r].size() && refSeedIndex < refSeeds.size()) {
+            std::cerr << readSeeds[r][readSeedIndex].seq << " vs " << refSeeds[refSeedIndex].seq << "\n";
+
+            if (readSeeds[r][readSeedIndex].seq < refSeeds[refSeedIndex].seq) {
+                std::cerr << "A\n";
+                readSeedIndex++;
+            } else if (readSeeds[r][readSeedIndex].seq > refSeeds[refSeedIndex].seq) {
+                std::cerr << "B\n";
+                refSeedIndex++;
+            } else {
+                std::cerr << "C\n";
+                matchingSeeds.push_back(readSeeds[r][readSeedIndex]);
+                readSeedIndex++;
+                refSeedIndex++;
+            }
+        }
+        readSeeds[r] = matchingSeeds;
+    }
+    
+    std::cerr << "MATCHED--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+
+    for(int i = 0; i < readSequences.size(); i++) {
+        std::cerr << "\n" << i <<"\n";
+        std::cerr << readSequences[i] << "\n";
+        std::cerr << readQuals[i] << "\n";
+        std::cerr << readSeeds[i].size() << "\n";
+        for(int j = 0; j < readSeeds[i].size() ; j++){
+            std::cerr << "XXXXXXXXXXXXXXXXXXXXXXXX\n";
+            std::cerr << readSeeds[i][j].seq.size() << "\n";
+            std::cerr << readSeeds[i][j].seq << "\n";
+            std::cerr << readSeeds[i][j].pos << "\n";
+            std::cerr << readSeeds[i][j].reversed << "\n";
+        }
+    }
+    
+
+
+
+    //TODO SPLIT THIS FUNCTION APART
+
+    
+    //C++ to C interface will have to be cleaned up
+    const char *reference = bestMatchSequence.c_str();
+    int n_reads = readSequences.size();
+    const char **read_strings = (const char **)malloc(n_reads*sizeof(char *));
+    const char **qual_strings = (const char **)malloc(n_reads*sizeof(char *));
+    const char **read_names = (const char **)malloc(n_reads*sizeof(char *));
+
+    int *r_lens         = (int *)malloc(n_reads*sizeof(int));
+    int *seed_counts    = (int *)malloc(n_reads*sizeof(int));
+
+    uint8_t **reversed  = (uint8_t **)malloc(n_reads*sizeof(uint8_t *));
+    int **ref_positions = (int **)malloc(n_reads*sizeof(int *));
+    int **qry_positions = (int **)malloc(n_reads*sizeof(int *));
+    
+
+    for(int i = 0; i < n_reads; i++) {
+        int n_seeds = readSeeds[i].size();
+
+        seed_counts[i] = n_seeds;
+        read_strings[i] = readSequences[i].c_str();
+        qual_strings[i] = readQuals[i].c_str();
+        read_names[i] = readNames[i].c_str();
+
+        r_lens[i] = readSequences[i].length();
+
+        uint8_t *reversed_array = (uint8_t *)malloc(n_seeds*sizeof(uint8_t));
+        int *ref_pos_array = (int *)malloc(n_seeds*sizeof(int));
+        int *qry_pos_array = (int *)malloc(n_seeds*sizeof(int));
+
+
+        for(int j = 0; j < n_seeds; j++){
+            reversed_array[j] = readSeeds[i][j].reversed;
+            qry_pos_array[j] = readSeeds[i][j].pos;
+            ref_pos_array[j] = seedToRefPositions[readSeeds[i][j].seq][0] + k - 1;
+        }
+
+        reversed[i]      = reversed_array;
+        ref_positions[i] = ref_pos_array;
+        qry_positions[i] = qry_pos_array;
+    }
+    
+    //std::cout << "\n@SQ	SN:reference	LN:" << ref_seq.length() << std::endl;  // sam header
+
+    char *sam_alignments[n_reads]; //constituants must be freed
+
+
+
+    for(int i = 0; i < n_reads; i++) {
+        std::cerr << "\n" << i <<"\n";
+        std::cerr << readSequences[i] << "\n";
+        std::cerr << readQuals[i] << "\n";
+        std::cerr << readSeeds[i].size() << "\n";
+        for(int j = 0; j < readSeeds[i].size() ; j++){
+            std::cerr << readSeeds[i][j].pos << "\n";
+            std::cerr << readSeeds[i][j].reversed << "\n";
+        }
+    }
+
+    align_reads(reference, n_reads, read_strings,qual_strings, read_names, r_lens, seed_counts, reversed, ref_positions, qry_positions, sam_alignments);
+    
+    
+    for(int i = 0; i < n_reads; i++) {
+        if (sam_alignments[i]) {
+        std::cout << sam_alignments[i] << std::endl;
+        }
+        
+        free(sam_alignments[i]);
+        free(reversed[i]);
+        free(ref_positions[i]);
+        free(qry_positions[i]);
+    }
+    free(qry_positions);
+    free(ref_positions);
+    free(reversed);
+    free(seed_counts);
+    free(r_lens);
+    free(read_strings);
+    free(qual_strings);
+    free(read_names);
 }
