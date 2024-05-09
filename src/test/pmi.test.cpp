8b83826165496fec1ffb4d82dbe76426e6d5f8e6
AlanZhangUCSC <bzhan146@ucsc.edu>
2024-05-09 15:30:06 -0700

    fixed most

--- src/test/pmi.test.cpp
+++ src/test/pmi.test.cpp
@@ -1,6 +1,15 @@
 #include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
+#include <iostream>
+#include <string>
+#include "PangenomeMAT.hpp"
+#include "../seeding.hpp"
+#include "../tree.hpp"
 #include "../pmi.hpp"
 
+
+
+namespace fs = boost::filesystem;
 
 BOOST_AUTO_TEST_CASE(_removeIndices) {
 /* should erase elements at the given positions */
@@ -35,6 +44,167 @@ BOOST_AUTO_TEST_CASE(_removeIndices) {
 
 }
 
+std::vector<std::tuple<std::string, int, int>> extractSeedmers(const std::string& seq, const int k, const int s, const int l, const bool open);
+void writeSeedmersFromIndexFile(seedmerIndex_t& index, std::unordered_map<int32_t, std::string> &seedmers, Tree *T, const Node *node, const std::string& testDirName);
+
+BOOST_AUTO_TEST_CASE(seedmersIndex) {
+    // construct true seeds
+    std::ifstream ifs("../dev/examples/sars2k.pmat");
+    boost::iostreams::filtering_streambuf< boost::iostreams::input> b;
+    b.push(boost::iostreams::gzip_decompressor());
+    b.push(ifs);
+    std::istream is(&b);
+    auto T = new PangenomeMAT::Tree(is);
+
+    // std::vector<std::tuple<int, int, int>> parameters = {{10, 5, 2}, {10, 5, 1}, {15, 7, 3}, {15, 7, 1}};
+    std::vector<std::tuple<int, int, int>> parameters = {{10, 5, 1}};
+
+    for (const auto& param : parameters) {
+        auto k = std::get<0>(param);
+        auto s = std::get<1>(param);
+        auto l = std::get<2>(param);
+
+        std::string dirName = std::string("../src/test/data/pmi_test/true_seedmers") + "_k" + std::to_string(k) + "_s" + std::to_string(s) + "_l" + std::to_string(l) + "/";
+        fs::create_directories(dirName);
+        std::cerr << "Printing seedmers from sequence, k: " << k << ", s: " << s << ", l: " << l << " in " << dirName << std::endl;
+        for (const auto& n : T->allNodes) {
+            std::string node_idn = n.first;
+            std::string outSeedmersPath = dirName + node_idn + ".pmi";
+            if (fs::exists(outSeedmersPath)) continue;
+
+            std::string node_seq = T->getStringFromReference(node_idn, false);
+            std::vector<std::tuple<std::string, int, int>> seedmers = extractSeedmers(node_seq, k, s, l, false);
+        
+            std::ofstream osdmfs(outSeedmersPath);
+            for (const auto& seedmer : seedmers) {
+                osdmfs << std::get<0>(seedmer) << "\t"
+                        << std::get<1>(seedmer) << "\t"
+                        << std::get<2>(seedmer) << std::endl;
+            }
+            osdmfs.close();
+        }
+
+        std::string testDirName = std::string("../src/test/data/pmi_test/test_seedmers") + "_k" + std::to_string(k) + "_s" + std::to_string(s) + "_l" + std::to_string(l) + "/";
+        std::cerr << "Building seedmers from index, k: " << k << ", s: " << s << ", l: " << l << " in " << dirName << std::endl;
+
+        pmi::seedIndex index;
+        pmi::build(index, T, l, k, s);
+        std::string indexFile = std::string("../src/test/data/pmi_test/") + "k" + std::to_string(k) + "_s" + std::to_string(s) + "_l" + std::to_string(l) + ".pmi";
+        std::ofstream fout(indexFile);
+        fout << index.outStream.str();
+        fout.close();
+        fs::create_directories(testDirName);
+
+        std::ifstream indexIf(indexFile);
+        std::string line;
+        std::getline(indexIf, line);
+        std::vector<std::string> spltTop;
+        PangenomeMAT::stringSplit(line, ' ', spltTop);
+        int32_t tempK = std::stoi(spltTop[0]);
+        int32_t tempS = std::stoi(spltTop[1]);
+        int32_t tempL = std::stoi(spltTop[2]);
+
+        seedmerIndex_t seedmerIndex;
+        while (std::getline(indexIf, line)) {
+            std::vector<std::string> splt;
+            stringSplit(line, ' ', splt);
+            std::string nid = splt[0];
+            seedmerIndex[nid] = {};
+            for (int32_t i = 1; i < splt.size(); i++) {
+                std::vector<std::string> splt2;
+                stringSplit(splt[i], ':', splt2);
+                int32_t pos = std::stoi(splt2[0]);
+                std::string seedmer = splt2[1];
+                seedmerIndex[nid].insert(std::make_pair(pos, seedmer));
+            }
+        }
+
+        std::cerr << "Printing seedmers from index, k: " << k << ", s: " << s << ", l: " << l << " in " << dirName << std::endl;
+
+        std::unordered_map<int32_t, std::string> dynamicSeedmersTarget;
+        writeSeedmersFromIndexFile(seedmerIndex, dynamicSeedmersTarget, T, T->root, testDirName);
+    }
+
+}
+
+static void mutateSeedmerMap(std::unordered_map<int32_t, std::string> &seedmers, const std::string &nid, seedmerIndex_t &index, std::vector<std::pair<int32_t, std::string>> &seedmersToRevert) {
+    // std::cout << "Forward => " << nid << "\n";
+    for (const auto &op : index[nid]) {
+        std::string seedmer = op.second;
+        int32_t pos = op.first;
+        if (seedmer[0] == '@') { // deletion
+            if (seedmers.find(pos) != seedmers.end()) {
+                seedmers.erase(seedmers.find(pos));
+            }
+        } else { // insertion
+            seedmersToRevert.push_back({pos, seedmers[pos]});
+            seedmers[pos] = seedmer;
+        }
+    }
+}
+
+static void revertSeedmerMap(std::unordered_map<int32_t, std::string> &seedmers, const std::string &nid, seedmerIndex_t &index, std::vector<std::pair<int32_t, std::string>> &seedmersToRevert) {
+    for (auto it = index[nid].rbegin(); it != index[nid].rend(); it++) {
+        std::string seedmer = it->second;
+        int32_t pos = it->first;
+        if (seedmer[0] == '@') { // deletion
+            seedmers[pos] = seedmer.substr(1);
+        } else {
+            if (seedmers.find(pos) != seedmers.end()) {
+                seedmers.erase(seedmers.find(pos));
+            }
+        }
+    }
+    for (const auto &p : seedmersToRevert) {
+        if (p.second.size() > 1) {
+            seedmers[p.first] = p.second;
+        }
+    }
+}
+
+void writeSeedmersFromIndexFile(seedmerIndex_t& index, std::unordered_map<int32_t, std::string> &seedmers, Tree *T, const Node *node, const std::string& testDirName) {
+    std::vector<std::pair<int32_t, std::string>> seedmersToRevert;
+    mutateSeedmerMap(seedmers, node->identifier, index, seedmersToRevert);
+
+    std::vector<std::pair<int32_t, std::string>> seedmersToWrite;
+    seedmersToWrite.reserve(seedmers.size());
+    for (const auto& s : seedmers) seedmersToWrite.emplace_back(std::make_pair(s.first, s.second));
+
+    std::sort(seedmersToWrite.begin(), seedmersToWrite.end(), [](const auto& a, const auto& b) {
+        return a.first < b.first;
+    });
+
+    std::ofstream osdmfs(testDirName + node->identifier + ".pmi");
+    for (const auto& seedmer : seedmersToWrite) {
+        osdmfs << seedmer.second << "\t" << seedmer.first << std::endl;
+    }
+    osdmfs.close();
+
+    for (Node *child : node->children) {
+        writeSeedmersFromIndexFile(index, seedmers, T, child, testDirName);
+    }
+
+    revertSeedmerMap(seedmers, node->identifier, index, seedmersToRevert);
+}
+std::vector<std::tuple<std::string, int, int>> extractSeedmers(const std::string& seq, const int k, const int s, const int l, const bool open) {
+    std::vector<std::tuple<std::string, int, int>> syncmers;
+
+    for (size_t i = 0; i < seq.size() - k + 1; ++i) {
+        std::string kmer = seq.substr(i ,k);
+        if (seeding::is_syncmer(kmer, s, open)) {
+            syncmers.emplace_back(std::make_tuple(kmer, i, i + k -1));
+        }
+    }
+
+    std::vector<std::tuple<std::string, int, int>> seedmers;
+    for (size_t i = 0; i < syncmers.size() - l + 1; ++i) {
+        std::string seedmerSeq = "";
+        for (size_t j = 0; j < l; ++j) seedmerSeq += std::get<0>(syncmers[i+j]);
+        seedmers.emplace_back(std::make_tuple(seedmerSeq, std::get<1>(syncmers[i]), std::get<2>(syncmers[i+l-1])));
+    }
+
+    return seedmers;
+}
 // BOOST_AUTO_TEST_CASE(_getRecomputePositions)
 // {
 //     using namespace tree;
