b5b325284f4cbc0057cb80506f7de210aa9f28ff
Alex Kramer <almkrame@ucsc.edu>
2024-08-30 21:54:20 +0000

    fix: allow Flask app to accept connections from any host to resolve HTTP 403 error

--- web_server/app.py
+++ web_server/app.py
@@ -49,7 +49,7 @@ def render_genome(genome, seeds):
     return rendered
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 @app.route('/data')
 def get_data():
