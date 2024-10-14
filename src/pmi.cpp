c3a0a01de68640ed00e877814ed581903d4d27c9
Alan <bzhan146@ucsc.edu>
2024-10-14 14:07:13 -0700

    minor

--- src/pmi.cpp
+++ src/pmi.cpp
@@ -2521,6 +2521,7 @@ void place_per_read_DFS(
       }
 
       std::vector<std::pair<uint32_t, std::vector<int32_t>>> readToAffectedSeedmerIndexVec;
+      readToAffectedSeedmerIndexVec.reserve(readToAffectedSeedmerIndex.size());
       for (const auto& [readIndex, affectedSeedmerIndices] : readToAffectedSeedmerIndex) {
           readToAffectedSeedmerIndexVec.emplace_back(readIndex, affectedSeedmerIndices);
       }
