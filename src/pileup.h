200254cb0970231c6f323756bf041b7204247eb0
amkram <almkrame@ucsc.edu>
2026-04-07 12:45:00 -0700

    replace samtools headers with samtools_compat.h

--- src/pileup.h
+++ src/pileup.h
@@ -1,12 +1,4 @@
-#include <htslib/cram.h>
-#include <htslib/faidx.h>
-#include <htslib/hts.h>
-#include <htslib/khash_str2int.h>
-#include <htslib/klist.h>
-#include <htslib/kstring.h>
-#include <htslib/sam.h>
-#include <samtools/bam_plcmd.h>
-#include <samtools/samtools.h>
+#include "samtools_compat.h"
 
 void bam_and_ref_to_mplp(sam_hdr_t *header, bam1_t **bam_lines, int nbams,
                          char *ref_string, int lref, kstring_t *mplp_string);
